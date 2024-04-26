6ef5b3912f95e47c589c1ead1fb8988a98f7749b
jiohjung98 <ckaquz98@naver.com>
2024-04-26 16:48:19 +0900

    temporary: 소셜로그인 기능(해결x)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,32 @@
 'use client';
 
+import axios from 'axios'; 
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+
+const frontendUrl = 'http://localhost:3000'; // 프론트엔드 도메인 URL
+const backendUrl = 'http://localhost:8080'; // 백엔드 서버 URL
+
+// const instance = axios.create({
+//   baseURL: backendUrl, // 백엔드 서버 URL 설정
+//   withCredentials: true, // CORS 정책 우회를 위해 withCredentials 옵션 true로 설정
+// });
 
 const LoginPage = () => {
   const router = useRouter();
   const [accessToken, setAccessToken] = useState('');
 
   const handleSocialLogin = async (providerType: string) => {
+    const redirectUri = `${frontendUrl}/login`; // 로그인 후 리다이렉트할 주소
     try {
-      const redirectUri = window.location.href;
-      const response = await fetch(`http://localhost:8080/oauth2/authorize/${providerType}?redirect_uri=${redirectUri}`);
-      const data = await response.json();
-      const receivedAccessToken = data.accessToken;
-      setAccessToken(receivedAccessToken);
-      router.push('/user/profile');
+      const response = await axios.get(`http://localhost:8080/oauth2/authorize/${providerType}?redirect_uri=${redirectUri}`);
+      const { accessToken } = response.data;
+      setAccessToken(accessToken);
+      console.log(response.data);
+      console.log(response);
+      console.log(response.data.result);
+      console.log(accessToken);
+      router.push('/login');
     } catch (error) {
       console.error('Error during social login:', error);
     }
