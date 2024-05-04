1e963f6f36b353d48b09352d6cd20ed40bdb9024
jiohjung98 <ckaquz98@naver.com>
2024-05-04 19:54:30 +0900

    design: 다음 버튼

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Swal from 'sweetalert2';
 import { checkEmailDuplicate } from '@/services/auth';
+import { useRouter } from 'next/navigation';
 
 interface SignUpFormProps {
   onSubmit: (data: { email: string; password: string }) => void;
@@ -18,6 +19,9 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
   const [passwordMatch, setPasswordMatch] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
   const [verificationClicked, setVerificationClicked] = useState(false);
+  const [agreementChecked, setAgreementChecked] = useState(false);
+
+  const router = useRouter();
 
   const handleEmailChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -76,6 +80,10 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
     onSubmit({ email, password });
   };
 
+  const handleAgreementChange = () => {
+    setAgreementChecked(!agreementChecked);
+  };
+
   return (
     <form onSubmit={handleSubmit} className="w-[80%] mx-auto mt-24">
       <div className='sign-up-info'>로그인 정보를 설정해주세요</div>
@@ -103,15 +111,25 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
           {passwordMatch && confirmPassword && <p className={`text-green-500`}>비밀번호가 일치합니다.</p>}
         </div>
       </div>
-      <button
-        type="submit"
-        className={`absolute left-1/2 transform -translate-x-1/2 bottom-32 w-56 h-16 bg-btn-color bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ${
-            !verificationClicked || !passwordValid || !passwordMatch ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400' : ''
-        }`}
-        disabled={!verificationClicked || !passwordValid || !passwordMatch}
-        >
-        다음
-      </button>
+      <div className="mb-4 flex items-center">
+        <input type="checkbox" id="agreement" checked={agreementChecked} onChange={handleAgreementChange} className="mr-2" />
+        <label htmlFor="agreement" className="text-black">
+          <span onClick={() => router.push('/serviceInfo')} style={{ color: 'gray', textDecoration: 'underline', cursor: 'pointer' }}>서비스 이용약관</span>과
+          {' '}
+          <span onClick={() => router.push('/privacy')} style={{ color: 'gray', textDecoration: 'underline', cursor: 'pointer' }}>개인정보 처리 방침</span>
+          에 동의합니다.
+        </label>
+      </div>
+      <div className="text-center">
+        <button
+          type="submit"
+          className={`mx-auto mt-32 mb-32 w-56 h-16 bg-btn-color bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ${
+          !verificationClicked || !passwordValid || !passwordMatch || !agreementChecked ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400' : ''
+          }`}
+          disabled={!verificationClicked || !passwordValid || !passwordMatch || !agreementChecked}>
+          다음
+        </button>
+      </div>
       {modalOpen && (
         <div className="modal">
           <div className="modal-content">
