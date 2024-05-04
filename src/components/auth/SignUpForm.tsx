c0e486a595f65fbf36305ea05059f984317fa473
jiohjung98 <ckaquz98@naver.com>
2024-05-04 19:24:48 +0900

    design: 버튼 background-color&radius

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -16,13 +16,12 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
   const [passwordValid, setPasswordValid] = useState(false);
   const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
   const [passwordMatch, setPasswordMatch] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false); // 모달 열림 여부
-  const [verificationClicked, setVerificationClicked] = useState(false); // 인증하기 버튼 클릭 여부
+  const [modalOpen, setModalOpen] = useState(false);
+  const [verificationClicked, setVerificationClicked] = useState(false);
 
   const handleEmailChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setEmail(value);
-    // 이메일 유효성 검사
     const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
     setEmailValid(isValid);
     setEmailErrorMessage(isValid ? '' : '이메일 형식이 올바르지 않습니다.');
@@ -44,12 +43,11 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
   };
 
   const handleEmailVerification = async () => {
-    // 사용 가능한 이메일인 경우에만 실행
     if (duplicateMessage === '사용 가능한 이메일입니다.') {
-      setVerificationClicked(true); // 인증하기 버튼 클릭 처리
+      setVerificationClicked(true);
       Swal.fire({
         title: '이메일을 보냈습니다.',
-        icon: 'success', // 체크 이모티콘
+        icon: 'success',
       });
     }
   };
@@ -60,7 +58,6 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
     const isValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/.test(value);
     setPasswordValid(isValid);
     setPasswordErrorMessage(isValid ? '' : '비밀번호 형식이 맞지 않습니다.');
-    // 비밀번호 일치 여부 메시지 초기화
     setPasswordMatch(false);
   };
 
@@ -84,8 +81,8 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
       <div className='sign-up-info'>로그인 정보를 설정해주세요</div>
       <div className="mb-4 relative">
         <label htmlFor="email" className="sign-up-info block mt-16">이메일</label>
-        <input type="email" id="email" value={email} onChange={handleEmailChange} placeholder="이메일 입력" className="w-full px-4 py-2 mt-8 h-16 rounded border border-gray-300 focus:border-blue-500 focus:outline-none" />
-        <button type="button" onClick={handleEmailVerification} disabled={!emailValid || duplicateMessage !== '사용 가능한 이메일입니다.'} className={`absolute top-1/2 mt-0.5 transform -translate-y-1/2 right-0 mr-2 px-4 py-2 ${duplicateMessage === '사용 가능한 이메일입니다.' ? 'bg-black text-white hover:bg-gray-900 focus:outline-none focus:bg-gray-900' : 'bg-gray-400 text-gray-600 cursor-not-allowed'} rounded`}>인증하기</button>
+        <input type="email" id="email" value={email} onChange={handleEmailChange} placeholder="이메일 입력" className="w-full px-4 py-2 mt-8 h-16 rounded-xl border border-gray-300 focus:border-blue-500 focus:outline-none" />
+        <button type="button" onClick={handleEmailVerification} disabled={!emailValid || duplicateMessage !== '사용 가능한 이메일입니다.'} className={`absolute top-1/2 mt-0.5 transform -translate-y-1/2 right-0 mr-2 px-4 py-2 ${duplicateMessage === '사용 가능한 이메일입니다.' ? 'bg-black text-white hover:bg-gray-900 focus:outline-none focus:bg-gray-900' : 'bg-gray-400 text-white cursor-not-allowed'} rounded-lg`}>인증하기</button>
         <div className='h-12'>
           <p className="text-red-500 mt-1">{emailErrorMessage}</p>
           <p className="text-green-500 mt-1">{duplicateMessage}</p>
@@ -93,23 +90,28 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit }) => {
       </div>
       <div className="mb-4">
         <label htmlFor="password" className="sign-up-info block">비밀번호</label>
-        <input type="password" id="password" value={password} onChange={handlePasswordChange} placeholder="비밀번호 입력" className="w-full px-4 py-2 mt-8 h-16 rounded border border-gray-300 focus:border-blue-500 focus:outline-none" />
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} placeholder="비밀번호 입력" className="w-full px-4 py-2 mt-8 h-16 rounded-xl border border-gray-300 focus:border-blue-500 focus:outline-none" />
         <div className='h-12'>
           {passwordErrorMessage && <p className="text-red-500">{passwordErrorMessage}</p>}
         </div>
       </div>
       <div className="mb-4">
         <label htmlFor="confirmPassword" className="sign-up-info block">비밀번호 확인</label>
-        <input type="password" id="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} placeholder="비밀번호 재입력" className="w-full px-4 py-2 mt-8 h-16 rounded border border-gray-300 focus:border-blue-500 focus:outline-none" />
+        <input type="password" id="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} placeholder="비밀번호 재입력" className="w-full px-4 py-2 mt-8 h-16 rounded-xl border border-gray-300 focus:border-blue-500 focus:outline-none" />
         <div className='h-12'>
           {confirmPassword && !passwordMatch && <p className={`text-red-500`}>비밀번호가 일치하지 않습니다.</p>}
           {passwordMatch && confirmPassword && <p className={`text-green-500`}>비밀번호가 일치합니다.</p>}
         </div>
       </div>
-      <button type="submit" className={`w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ${!verificationClicked || !passwordValid || !passwordMatch ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400' : ''}`} disabled={!verificationClicked || !passwordValid || !passwordMatch}>회원가입</button>
-      
-      {/* 모달 */}
-      {/* 모달 */}
+      <button
+        type="submit"
+        className={`absolute left-1/2 transform -translate-x-1/2 bottom-32 w-56 h-16 bg-btn-color bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ${
+            !verificationClicked || !passwordValid || !passwordMatch ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400' : ''
+        }`}
+        disabled={!verificationClicked || !passwordValid || !passwordMatch}
+        >
+        다음
+      </button>
       {modalOpen && (
         <div className="modal">
           <div className="modal-content">
