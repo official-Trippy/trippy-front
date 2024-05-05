cf51586ebf8429831fdd01910fb9ef4643861753
jiohjung98 <ckaquz98@naver.com>
2024-05-05 17:39:55 +0900

    블로그 닉네임 및 이름 중복 체크 api

---
+++ src/services/blog.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export async function checkNickNameDuplicate(nickName: string) {
+    try {
+      const response = await axios.get(`${backendUrl}/api/member/isDuplicated?nickName=${nickName}`);
+      const data = response.data;
+      console.log(response);
+      console.log(data);
+      if (data.isSuccess && data.result && data.result.isDuplicated) {
+        return { duplicated: true };
+      } else {
+        return { duplicated: false };
+      }
+    } catch (error) {
+      throw new Error(`Error checking email duplication: ${error}`);
+    }
+}
+
+export async function checkBlogDuplicate(blogName: string) {
+    try {
+      const response = await axios.get(`${backendUrl}/api/member/isDuplicated?blogName=${blogName}`);
+      const data = response.data;
+      console.log(response);
+      console.log(data);
+      if (data.isSuccess && data.result && data.result.isDuplicated) {
+        return { duplicated: true };
+      } else {
+        return { duplicated: false };
+      }
+    } catch (error) {
+      throw new Error(`Error checking email duplication: ${error}`);
+    }
+}
