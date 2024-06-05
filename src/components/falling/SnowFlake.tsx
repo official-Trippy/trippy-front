7233cf12d321821213078228f18853640f66fc4c
kimkimjunjun <wnsgnl323@gmail.com>
2024-06-05 20:54:45 +0900

    feat:pull data

--- src/components/falling/SnowFlake.tsx
+++ src/components/falling/SnowFlake.tsx
@@ -1,24 +1,23 @@
 type Props = {
-    animationDelay: string;
-    fontSize: string;
-    colorDepth: number;
-  };
-  
-  export default function SnowFlake({
-    animationDelay,
-    fontSize,
-    colorDepth,
-  }: Props) {
-    return (
-      <p
-        className={`animate-[fall_30s_infinite]  opacity-0 ${
-          { 0: 'text-blue-100', 1: 'text-blue-200', 2: 'text-blue-300' }[
-            colorDepth
-          ]
+  animationDelay: string;
+  fontSize: string;
+  colorDepth: number;
+};
+
+export default function SnowFlake({
+  animationDelay,
+  fontSize,
+  colorDepth,
+}: Props) {
+  return (
+    <p
+      className={`animate-[fall_30s_infinite]  opacity-0 ${{ 0: 'text-blue-100', 1: 'text-blue-200', 2: 'text-blue-300' }[
+        colorDepth
+        ]
         }`}
-        style={{ animationDelay, fontSize }}
-      >
-        {'\u2745'}
-      </p>
-    );
-  }
+      style={{ animationDelay, fontSize }}
+    >
+      {'\u2745'}
+    </p>
+  );
+}
