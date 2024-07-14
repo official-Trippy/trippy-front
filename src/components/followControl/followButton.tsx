62449caab91ca7e6e4e958bea86db490130f1e74
yunhwancho <jyh6314@naver.com>
2024-07-15 02:13:54 +0900

    feat : 팔로우/언팔로우 전역상태 관리에 따른 구분 기능 추가

---
+++ src/components/followControl/followButton.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { doFollow, unfollow } from "@/services/follow";
+import { useUserStore } from "@/store/useUserStore";
+
+const FollowButton: React.FC<{ targetMemberId: string }> = ({
+  targetMemberId,
+}) => {
+  const queryClient = useQueryClient();
+  const userInfo = useUserStore((state) => state.userInfo);
+  const isFollowing = userInfo?.following.some(
+    (following) => following.memberId === targetMemberId
+  );
+
+  const followMutation = useMutation(() => doFollow(targetMemberId), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("userInfo");
+    },
+  });
+
+  const unfollowMutation = useMutation(() => unfollow(targetMemberId), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("userInfo");
+    },
+  });
+
+  const handleFollowClick = () => {
+    if (isFollowing) {
+      unfollowMutation.mutate();
+    } else {
+      followMutation.mutate();
+    }
+  };
+
+  return (
+    <button onClick={handleFollowClick}>
+      {isFollowing ? "Unfollow" : "Follow"}
+    </button>
+  );
+};
+
+export default FollowButton;
