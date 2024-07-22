689df74f730684e7d3f43617d9b27080fbca5542
jiohjung98 <ckaquz98@naver.com>
2024-07-22 16:01:06 +0900

    design: md 800px로 변경

--- src/components/pages/ootd/RecentOotdPost.tsx
+++ src/components/pages/ootd/RecentOotdPost.tsx
@@ -10,7 +10,7 @@ import { fetchAllOotdPostCount, fetchAllOotdPosts } from '@/services/ootd.ts/oot
 import { MemberInfo } from '@/services/auth';
 import { formatDate } from '@/constants/dateFotmat';
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 8;
 
 const RecentOotdPost: React.FC = () => {
   const accessToken = Cookies.get('accessToken');
