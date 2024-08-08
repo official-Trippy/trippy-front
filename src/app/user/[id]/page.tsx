bfb0b94df468a249f34934200ec3b080b4c821ef
yunhwancho <jyh6314@naver.com>
2024-08-08 20:52:21 +0900

    feat : 다른 유저 페이지 팔로우 기능api 연동

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,24 +1,25 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import { useQuery } from 'react-query';
-import Header from '@/components/shared/header/Header';
-import UserProfile from '@/components/user/UserProfile';
-import UserOotd from '@/components/user/UserOotd';
-import UserTicket from '@/components/user/UserTicket';
-import UserBadge from '@/components/user/UserBadge';
-import UserBookmark from '@/components/user/UserBookmark'
-import Image from 'next/image';
-import backgroundImg from '../../../../public/DefaultBackground.svg';
-import { fetchUserProfile } from '@/services/ootd.ts/ootdGet';
+import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import Header from "@/components/shared/header/Header";
+import UserProfile from "@/components/user/UserProfile";
+import UserOotd from "@/components/user/UserOotd";
+import UserTicket from "@/components/user/UserTicket";
+import UserBadge from "@/components/user/UserBadge";
+import UserBookmark from "@/components/user/UserBookmark";
+import Image from "next/image";
+import backgroundImg from "../../../../public/DefaultBackground.svg";
+import { fetchUserProfile } from "@/services/ootd.ts/ootdGet";
+import FollowList from "@/components/profile/FollowList";
 
 const TABS = {
-  TICKET: 'TICKET',
-  OOTD: 'OOTD',
-  BADGE: 'BADGE',
-  BOOKMARK: 'BOOKMARK',
-  FOLLOWER: 'FOLLOWER',
-  FOLLOWING: 'FOLLOWING',
+  TICKET: "TICKET",
+  OOTD: "OOTD",
+  BADGE: "BADGE",
+  BOOKMARK: "BOOKMARK",
+  FOLLOWER: "FOLLOWER",
+  FOLLOWING: "FOLLOWING",
 };
 
 const UserPage = ({ params }: { params: { id: string } }) => {
@@ -26,7 +27,7 @@ const UserPage = ({ params }: { params: { id: string } }) => {
   const [activeTab, setActiveTab] = useState(TABS.TICKET);
 
   const { data, error, isLoading, refetch } = useQuery({
-    queryKey: ['userProfile', id],
+    queryKey: ["userProfile", id],
     queryFn: () => fetchUserProfile(id),
     onError: (error) => {
       console.error(error);
@@ -46,8 +47,9 @@ const UserPage = ({ params }: { params: { id: string } }) => {
   }
 
   const userData = data && data.result;
-  console.log('userData: ', userData);
-  const member = userData.memberId;
+  console.log("userData: ", userData);
+  const member = userData.email;
+  console.log(member);
 
   return (
     <>
@@ -74,39 +76,65 @@ const UserPage = ({ params }: { params: { id: string } }) => {
             <div className="flex space-x-4">
               <button
                 className={`px-8 py-2 rounded-[999px] justify-center items-center ${
-                  activeTab === TABS.TICKET ? 'bg-[#ffe3ea] border-2 border-[#fa3463]' : 'border border-[#cfcfcf]'
+                  activeTab === TABS.TICKET
+                    ? "bg-[#ffe3ea] border-2 border-[#fa3463]"
+                    : "border border-[#cfcfcf]"
                 }`}
                 onClick={() => setActiveTab(TABS.TICKET)}
               >
-                <span className={activeTab === TABS.TICKET ? 'text-[#fa3463]' : ''}>티켓</span>
+                <span
+                  className={activeTab === TABS.TICKET ? "text-[#fa3463]" : ""}
+                >
+                  티켓
+                </span>
               </button>
               <button
                 className={`px-8 py-2 rounded-[999px] justify-center items-center ${
-                  activeTab === TABS.OOTD ? 'bg-[#ffe3ea] border-2 border-[#fa3463]' : 'border border-[#cfcfcf]'
+                  activeTab === TABS.OOTD
+                    ? "bg-[#ffe3ea] border-2 border-[#fa3463]"
+                    : "border border-[#cfcfcf]"
                 }`}
                 onClick={() => setActiveTab(TABS.OOTD)}
               >
-                <span className={activeTab === TABS.OOTD ? 'text-[#fa3463]' : ''}>OOTD</span>
+                <span
+                  className={activeTab === TABS.OOTD ? "text-[#fa3463]" : ""}
+                >
+                  OOTD
+                </span>
               </button>
               <button
                 className={`px-8 py-2 rounded-[999px] justify-center items-center ${
-                  activeTab === TABS.BADGE ? 'bg-[#ffe3ea] border-2 border-[#fa3463]' : 'border border-[#cfcfcf]'
+                  activeTab === TABS.BADGE
+                    ? "bg-[#ffe3ea] border-2 border-[#fa3463]"
+                    : "border border-[#cfcfcf]"
                 }`}
                 onClick={() => setActiveTab(TABS.BADGE)}
               >
-                <span className={activeTab === TABS.BADGE ? 'text-[#fa3463]' : ''}>뱃지</span>
+                <span
+                  className={activeTab === TABS.BADGE ? "text-[#fa3463]" : ""}
+                >
+                  뱃지
+                </span>
               </button>
               <button
                 className={`px-8 py-2 rounded-[999px] justify-center items-center ${
-                  activeTab === TABS.BOOKMARK ? 'bg-[#ffe3ea] border-2 border-[#fa3463]' : 'border border-[#cfcfcf]'
+                  activeTab === TABS.BOOKMARK
+                    ? "bg-[#ffe3ea] border-2 border-[#fa3463]"
+                    : "border border-[#cfcfcf]"
                 }`}
                 onClick={() => setActiveTab(TABS.BOOKMARK)}
               >
-                <span className={activeTab === TABS.BOOKMARK ? 'text-[#fa3463]' : ''}>북마크</span>
+                <span
+                  className={
+                    activeTab === TABS.BOOKMARK ? "text-[#fa3463]" : ""
+                  }
+                >
+                  북마크
+                </span>
               </button>
             </div>
             <div className="flex space-x-4">
-              <button
+              {/* <button
                 className={`pr-8 py-2 ${
                   activeTab === TABS.FOLLOWER ? 'text-rose-500 font-bold' : 'bg-white'
                 }`}
@@ -121,7 +149,7 @@ const UserPage = ({ params }: { params: { id: string } }) => {
                 onClick={() => setActiveTab(TABS.FOLLOWING)}
               >
                 팔로윙
-              </button>
+              </button> */}
             </div>
           </div>
           <hr className="mb-4 w-full h-[1px]" />
@@ -131,6 +159,12 @@ const UserPage = ({ params }: { params: { id: string } }) => {
             {activeTab === TABS.OOTD && <UserOotd memberId={id} />}
             {activeTab === TABS.BADGE && <UserBadge />}
             {activeTab === TABS.BOOKMARK && <UserBookmark />}
+            {activeTab === TABS.FOLLOWER && (
+              <FollowList memberId={member} type="follower" />
+            )}
+            {activeTab === TABS.FOLLOWING && (
+              <FollowList memberId={member} type="following" />
+            )}
           </div>
         </div>
       </div>
