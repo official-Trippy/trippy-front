553d025a6842644ad9390b74956bab615657781b
kimkimjunjun <wnsgnl323@gmail.com>
2024-08-13 14:22:25 +0900

    feat: editor 추가

--- src/components/testEditor/textEditor2.tsx
+++ src/components/testEditor/textEditor2.tsx
@@ -12,19 +12,48 @@ const MyTinyMCEEditor = () => {
         <div>
             <Editor
                 initialValue="<p>여러분의 경험을 자유롭게 적어주세요.</p>"
+                apiKey='a7jtjjd8h1f0ofvjye2cxg0xwzsz6576rgwoj91atcc2i8pf'
                 init={{
-                    height: 400,
+                    height: 820,
                     menubar: false,
                     plugins: [
-                        'advlist autolink lists link image charmap print preview anchor',
-                        'searchreplace visualblocks code fullscreen',
-                        'insertdatetime media table paste code help wordcount'
+                        'lists',
+                        'link',
+                        'image',
+                        'charmap',
+                        'preview',
+                        'searchreplace',
+                        'fullscreen',
+                        'media',
+                        'table',
+                        'code',
+                        'help',
+                        'emoticons',
+                        'codesample',
+                        'quickbars',
                     ],
                     toolbar:
-                        'styleselect | bold italic backcolor | \
-                        alignleft aligncenter alignright alignjustify | \
-                        bullist numlist outdent indent | link image | \
-                        removeformat | help | undo redo',
+
+                        'bold italic forecolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'lists table link charmap searchreplace | ' +
+                        'image media codesample emoticons fullscreen preview | ' +
+                        'removeformat | undo redo' +
+                        '',
+                    // setup(editor) {
+                    //     setTinymceEditor(editor); // activeEditor를 전역적으로 관리
+
+                    //     // 이미지를 업로드하는 버튼 추가
+                    //     editor.ui.registry.addButton('image-upload', {
+                    //       icon: 'image',
+                    //       tooltip: '업로드',
+                    //       onAction: () => {
+                    //         editor.execCommand('image-upload');
+                    //       },
+                    //     });
+
+                    //     editor.addCommand('image-upload', onOpenFile); // onOpenFile 함수 실행
+                    //   },
                 }}
                 onEditorChange={handleEditorChange}
             />
