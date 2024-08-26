fcbb7bbd320a9b40daa02b1f5c1123e0b7f44554
jiohjung98 <ckaquz98@naver.com>
2024-08-26 16:10:49 +0900

    design: 마이페이지 ootd body ...처리

--- src/components/profile/MyOotd.tsx
+++ src/components/profile/MyOotd.tsx
@@ -94,7 +94,9 @@ const MyOotd: React.FC<MyOotdProps> = ({ userInfo }) => {
               />
               <span className="mx-2 text-[#cfcfcf]"> {item.post.commentCount}</span>
             </div>
-            <div className="text-[#6b6b6b] text-xl font-normal font-['Pretendard']">{item.post.body}</div>
+            <div className="min-h-[35px] text-[#6b6b6b] text-xl font-normal font-['Pretendard'] text-ellipsis overflow-hidden whitespace-nowrap">
+              {item.post.body}
+            </div>
             <div className="tag-container">
               {item.post.tags.map((tag: string, index: number) => (
                 <span
