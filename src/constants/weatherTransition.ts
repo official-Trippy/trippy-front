d07f226cdec73da33eacd0eab9f73923246af202
jiohjung98 <ckaquz98@naver.com>
2024-09-24 20:01:16 +0900

    fix: weather Api 오류 해결

--- src/constants/weatherTransition.ts
+++ src/constants/weatherTransition.ts
@@ -15,3 +15,15 @@ export const getWeatherStatusInfo = (status: string) => {
 
   return weatherMap[status] || { label: '정보 없음', icon: sunnyIcon };
 };
+
+export const getWeatherStatusInKorean = (status: string) => {
+  const weatherMap: { [key: string]: string } = {
+    rain: '비',
+    snow: '눈',
+    mostly_cloudy: '구름많음',
+    cloudy: '흐림',
+    sunny: '맑음',
+  };
+
+  return weatherMap[status] || '정보 없음';
+};
