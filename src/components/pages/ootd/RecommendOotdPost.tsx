76b0eb8f2f1a563e98c22379bd24010c57108bb8
jiohjung98 <ckaquz98@naver.com>
2024-09-29 00:22:03 +0900

    fix: 메인 왼쪽 오른쪽 버튼 크기 조정

--- src/components/pages/ootd/RecommendOotdPost.tsx
+++ src/components/pages/ootd/RecommendOotdPost.tsx
@@ -363,10 +363,10 @@ const TagContainer: React.FC<TagContainerProps> = ({ item }) => {
            <Image
            src={SwiperLeftButton}
            alt="Previous"
-           width={60}
-           height={60}
+           width={40}
+           height={40}
            onClick={() => handleScrollOotd('left')}
-          className="absolute left-[-30px] top-[60%] transform -translate-y-1/2 z-10"
+          className="absolute left-[-22px] top-[61%] transform -translate-y-1/2 z-10"
          />
             )}
 
@@ -456,10 +456,10 @@ const TagContainer: React.FC<TagContainerProps> = ({ item }) => {
                 <Image
                     src={SwiperRightButton}
                     alt="Next"
-                    width={60}
-                    height={60}
+                    width={40}
+                    height={40}
                     onClick={() => handleScrollOotd('right')}
-                    className="absolute right-[-30px] top-[60%] transform -translate-y-1/2 z-10"
+                    className="absolute right-[-22px] top-[60%] transform -translate-y-1/2 z-10"
                 />
             )}
         </div>
