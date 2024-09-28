49691038a254c67bb1b835e3b0b06e051d0d9c1c
jiohjung98 <ckaquz98@naver.com>
2024-09-28 15:08:41 +0900

    feat: 추천 관광지 클릭 시, 카카오맵 이동

--- src/components/ootd/RecommendedSpot.tsx
+++ src/components/ootd/RecommendedSpot.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -25,7 +25,6 @@ interface RecommendedSpotProps {
 
 const RecommendedSpot: React.FC<RecommendedSpotProps> = ({ recommendedSpots }) => {
   const [itemsPerSlide, setItemsPerSlide] = useState(4);
-  const scrollRef = useRef<HTMLDivElement | null>(null);
   const swiperRef = useRef<SwiperRef | null>(null);
 
   const updateItemsPerSlide = () => {
@@ -43,7 +42,6 @@ const RecommendedSpot: React.FC<RecommendedSpotProps> = ({ recommendedSpots }) =
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // 페이지가 로드될 때, 그리고 창 크기가 변경될 때마다 슬라이드 개수 업데이트
       updateItemsPerSlide();
       window.addEventListener('resize', updateItemsPerSlide);
 
@@ -53,36 +51,52 @@ const RecommendedSpot: React.FC<RecommendedSpotProps> = ({ recommendedSpots }) =
     }
   }, []);
 
-
   const handleScrollOotd = (direction: string) => {
     if (swiperRef.current) {
-        if (direction === 'left') {
-            swiperRef.current.swiper.slidePrev();
-        } else {
-            swiperRef.current.swiper.slideNext();
-        }
+      if (direction === 'left') {
+        swiperRef.current.swiper.slidePrev();
+      } else {
+        swiperRef.current.swiper.slideNext();
+      }
     }
   };
+
+  const handleSpotClick = (title: string) => {
+    const kakaoMapUrl = `https://map.kakao.com/link/search/${encodeURIComponent(title)}`;
+    window.open(kakaoMapUrl, '_blank');
+  };
+
+  // 호버 상태 관리
+  const [hoveredSpot, setHoveredSpot] = useState<number | null>(null);
+
+  const handleMouseEnter = (index: number) => {
+    setHoveredSpot(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredSpot(null);
+  };
+
   return (
-    <div className="relative w-[90%] sm-700:w-[66%] sm-700:max-w-7xl  mx-auto pt-[2rem] overflow-visible">
+    <div className="relative w-[90%] sm-700:w-[66%] sm-700:max-w-7xl mx-auto pt-[2rem] overflow-visible">
       <h2 className="font-bold text-2xl mb-4">추천 장소를 알려드릴게요!</h2>
       {itemsPerSlide < recommendedSpots.length && (
-      <Image
-                    src={SwiperLeftButton}
-                    alt="Previous"
-                    width={30}
-                    height={30}
-                    onClick={() => handleScrollOotd('left')}
-                    style={{
-                      width: '30px',
-                      height: '30px',
-                      position: 'absolute',
-                      top: '60%',
-                      transform: 'translateY(-50%)',
-                      zIndex: 999,
-                      left: '-10px'
-                  }}
-                />
+        <Image
+          src={SwiperLeftButton}
+          alt="Previous"
+          width={30}
+          height={30}
+          onClick={() => handleScrollOotd('left')}
+          style={{
+            width: '30px',
+            height: '30px',
+            position: 'absolute',
+            top: '60%',
+            transform: 'translateY(-50%)',
+            zIndex: 999,
+            left: '-10px',
+          }}
+        />
       )}
       <div className="relative mx-auto">
         <Swiper
@@ -94,33 +108,70 @@ const RecommendedSpot: React.FC<RecommendedSpotProps> = ({ recommendedSpots }) =
           {recommendedSpots.length > 0 ? (
             recommendedSpots.map((spot, index) => (
               <SwiperSlide key={index} className="flex flex-col items-center justify-center text-center">
-              <div className="rounded-lg py-4 flex items-center justify-center">
-                {spot.imgList && spot.imgList.length > 0 ? (
-                  <div className="w-[158px] h-[100px] flex justify-center items-center">
+                <div
+                  className="relative rounded-lg py-4 flex items-center justify-center cursor-pointer"
+                  onClick={() => handleSpotClick(spot.title)}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {spot.imgList && spot.imgList.length > 0 ? (
+                    <div className="relative w-[158px] h-[100px] flex justify-center items-center rounded-[8px] overflow-hidden">
                     <Image
                       src={spot.imgList[0].galWebImageUrl}
                       alt={spot.title}
                       width={158}
                       height={100}
-                      className="rounded-[8px] mt-2 object-cover"
+                      className="rounded-[8px] object-cover"
                       style={{ width: '158px', height: '100px' }}
                     />
+                    {/* 호버 시 "위치 확인하기" 텍스트 */}
+                    {hoveredSpot === index && (
+                      <div
+                        className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+                        style={{ 
+                          WebkitMaskImage: 'linear-gradient(white, white)',  // mask 속성으로 테두리를 둥글게 적용
+                          maskImage: 'linear-gradient(white, white)',        // 다양한 브라우저 지원을 위해 prefix 사용
+                          borderRadius: '8px',                              // 둥근 모서리
+                        }}
+                      >
+                        <span className="text-white text-sm font-bold">위치 확인하기</span>
+                      </div>
+                    )}
                   </div>
-                ) : (
-                  <div className="w-[158px] h-[100px] flex justify-center items-center">
-                    <Image
-                      src={DefaultImage}
-                      alt="이미지가 없습니다."
-                      width={158}
-                      height={100}
-                      className="rounded-[8px] mt-2 object-cover"
-                      style={{ width: '158px', height: '100px' }}
-                    />
-                  </div>
-                )}
-              </div>
-              <p className="text-xl font-medium">{spot.title}</p>
-            </SwiperSlide>
+                  ) : (
+                    <div className="relative w-[158px] h-[100px] flex justify-center items-center">
+                      <Image
+                        src={DefaultImage}
+                        alt="이미지가 없습니다."
+                        width={158}
+                        height={100}
+                        className="rounded-[8px] object-cover"
+                        style={{ width: '158px', height: '100px' }}
+                      />
+                      {hoveredSpot === index && (
+                      <div
+                        className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+                        style={{ 
+                          WebkitMaskImage: 'linear-gradient(white, white)',  // mask 속성으로 테두리를 둥글게 적용
+                          maskImage: 'linear-gradient(white, white)',        // 다양한 브라우저 지원을 위해 prefix 사용
+                          borderRadius: '8px',                              // 둥근 모서리
+                        }}
+                      >
+                        <span className="text-white text-sm font-bold">위치 확인하기</span>
+                      </div>
+                    )}
+                    </div>
+                  )}
+                </div>
+                <p
+                  className="text-xl font-medium cursor-pointer"
+                  onClick={() => handleSpotClick(spot.title)}
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {spot.title}
+                </p>
+              </SwiperSlide>
             ))
           ) : (
             <div className="text-center w-full">추천 장소가 없습니다.</div>
@@ -128,24 +179,23 @@ const RecommendedSpot: React.FC<RecommendedSpotProps> = ({ recommendedSpots }) =
         </Swiper>
       </div>
       {itemsPerSlide < recommendedSpots.length && (
-      <Image
-                    src={SwiperRightButton}
-                    alt="Next"
-                    width={30}
-                    height={30}
-                    onClick={() => handleScrollOotd('right')}
-                    style={{
-                      width: '30px',
-                      height: '30px',
-                      position: 'absolute',
-                      top: '60%', 
-                      transform: 'translateY(-50%)',
-                      right: '-10px',
-                      zIndex: 999,
-                  }}
-                />
+        <Image
+          src={SwiperRightButton}
+          alt="Next"
+          width={30}
+          height={30}
+          onClick={() => handleScrollOotd('right')}
+          style={{
+            width: '30px',
+            height: '30px',
+            position: 'absolute',
+            top: '60%',
+            transform: 'translateY(-50%)',
+            right: '-10px',
+            zIndex: 999,
+          }}
+        />
       )}
-
     </div>
   );
 };
