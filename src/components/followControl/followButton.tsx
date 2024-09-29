234a24bbd5c6cc417d194ed8a891589d76542a5c
jiohjung98 <ckaquz98@naver.com>
2024-09-29 23:57:01 +0900

    design: 팔로잉 버튼 텍스트 컬러

--- src/components/followControl/followButton.tsx
+++ src/components/followControl/followButton.tsx
@@ -75,7 +75,7 @@ const FollowButton: React.FC<FollowButtonProps> = ({
   return (
     <div className="min-w-[70px] flex items-center">
       <button
-        className={`bg-${localIsFollowing ? "[#F5F5F5]" : "[#FB3463]"} text-${localIsFollowing ? "[#292929]" : "white"} text-base text-white px-[2rem] py-[0.5rem] rounded-lg`}
+        className={`bg-${localIsFollowing ? "[#F5F5F5]" : "[#FB3463]"} text-${localIsFollowing ? "[#292929]" : "white"} text-base font-bold px-[2rem] py-[0.5rem] rounded-lg`}
         onClick={followHandler}
         disabled={loading}
       >
