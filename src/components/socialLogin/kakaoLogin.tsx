a527fe3dc093a79233ed4830c223c8e81b43e159
jiohjung98 <ckaquz98@naver.com>
2024-10-01 17:56:25 +0900

    fix: 소셜로그인 버튼 cursor pointer

--- src/components/socialLogin/kakaoLogin.tsx
+++ src/components/socialLogin/kakaoLogin.tsx
@@ -63,7 +63,7 @@ export default function KakaoLogin() {
   };
 
   return (
-    <div className="flex flex-col my-1 items-center">
+    <div className="flex flex-col my-1 items-center cursor-pointer">
       <div
         className="bg-kakao-btn text-neutral-900 dark:text-white  px-4 rounded-md my-2 w-[310px] h-[44px] flex items-center justify-center"
         onClick={kakaoLoginHandler}
